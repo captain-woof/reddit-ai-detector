ccd221669421d0bad258b45b32591c1bbc329dc4
captainwoof <captainwoof.official@gmail.com>
2024-08-04 16:04:31 +0000

    Result messages improv

--- utils/result.py
+++ utils/result.py
@@ -9,21 +9,23 @@ Scoring scheme
 """
 
 message1 = """
-# LLM post detector results
+# AI-generated text detection result
 
-**This post does not contain LLM-generated texts.**
+**This post does not contain AI-generated texts.**
 
 LLMs like ChatGPT can generate texts in mass quantities, which can be very elaborate and convincing - almost human-like. This is often used by scammers nowadays to fool people into believing that they are interacting with a real person. This escalates into all sorts of scams, including financial, identity, and much more.
 
 I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts.
 
 *If you think this result is a mistake, or have suggestions, please DM me.*
+
+Bot author: [@realCaptainWoof](https://x.com/realCaptainWoof) | Powered by [ZeroGPT](https://www.zerogpt.com/)
 """
 
 message2 = """
-# LLM post detector results
+# AI-generated text detection result
 
-**This post contains a few LLM-generated texts (between 1% - 25%).** The LLM-generated texts are:
+**This post contains a few AI-generated texts (between 1% - 25%).** The AI-generated texts are:
 {0}
 
 LLMs like ChatGPT can generate texts in mass quantities, which can be very elaborate and convincing - almost human-like. This is often used by scammers nowadays to fool people into believing that they are interacting with a real person. This escalates into all sorts of scams, including financial, identity, and much more.
@@ -31,12 +33,14 @@ LLMs like ChatGPT can generate texts in mass quantities, which can be very elabo
 I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts.
 
 *If you think this result is a mistake, please DM me.*
+
+Bot author: [@realCaptainWoof](https://x.com/realCaptainWoof) | Powered by [ZeroGPT](https://www.zerogpt.com/)
 """
 
 message3 = """
-# LLM post detector results
+# AI-generated text detection result
 
-**This post contains some LLM-generated texts (between 26% - 50%).** The LLM-generated texts are:
+**This post contains some AI-generated texts (between 26% - 50%).** The AI-generated texts are:
 {0}
 
 LLMs like ChatGPT can generate texts in mass quantities, which can be very elaborate and convincing - almost human-like. This is often used by scammers nowadays to fool people into believing that they are interacting with a real person. This escalates into all sorts of scams, including financial, identity, and much more.
@@ -44,45 +48,53 @@ LLMs like ChatGPT can generate texts in mass quantities, which can be very elabo
 I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts.
 
 *If you think this result is a mistake, please DM me.*
+
+Bot author: [@realCaptainWoof](https://x.com/realCaptainWoof) | Powered by [ZeroGPT](https://www.zerogpt.com/)
 """
 
 message4 = """
-# LLM post detector results
+# AI-generated text detection result
 
-**This post contains a lot of LLM-generated texts (between 51% - 75%).** The LLM-generated texts are:
+**This post contains a lot of AI-generated texts (between 51% - 75%).** The AI-generated texts are:
 {0}
 
 LLMs like ChatGPT can generate texts in mass quantities, which can be very elaborate and convincing - almost human-like. This is often used by scammers nowadays to fool people into believing that they are interacting with a real person. This escalates into all sorts of scams, including financial, identity, and much more.
 
-I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts.
+I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts. In addition, I downvote posts like these, with a high percentage of fake texts, on behalf of my creator.
 
 *If you think this result is a mistake, please DM me.*
+
+Bot author: [@realCaptainWoof](https://x.com/realCaptainWoof) | Powered by [ZeroGPT](https://www.zerogpt.com/)
 """
 
 message5 = """
-# LLM post detector results
+# AI-generated text detection result
 
-**This post contains mostly LLM-generated texts (between 76% - 99%).** The LLM-generated texts are:
+**This post contains mostly AI-generated texts (between 76% - 99%).** The AI-generated texts are:
 {0}
 
 LLMs like ChatGPT can generate texts in mass quantities, which can be very elaborate and convincing - almost human-like. This is often used by scammers nowadays to fool people into believing that they are interacting with a real person. This escalates into all sorts of scams, including financial, identity, and much more.
 
-I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts.
+I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts. In addition, I downvote posts like these, with a high percentage of fake texts, on behalf of my creator.
 
 *If you think this result is a mistake, please DM me.*
+
+Bot author: [@realCaptainWoof](https://x.com/realCaptainWoof) | Powered by [ZeroGPT](https://www.zerogpt.com/)
 """
 
 message6 = """
-# LLM post detector results
+# AI-generated text detection result
 
-**This post is entirely LLM-generated.** The LLM-generated texts are:
+**This post is entirely AI-generated.** The AI-generated texts are:
 {0}
 
 LLMs like ChatGPT can generate texts in mass quantities, which can be very elaborate and convincing - almost human-like. This is often used by scammers nowadays to fool people into believing that they are interacting with a real person. This escalates into all sorts of scams, including financial, identity, and much more.
 
-I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts.
+I was created to check some vulnerable subreddits for such posts, and alert other redditors if I see LLM-generated posts. In addition, I downvote posts like these, with a high percentage of fake texts, on behalf of my creator.
 
 *If you think this result is a mistake, please DM me.*
+
+Bot author: [@realCaptainWoof](https://x.com/realCaptainWoof) | Powered by [ZeroGPT](https://www.zerogpt.com/)
 """
 
 
